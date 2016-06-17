8eaf1321efea603dbd3d557ffe37464562c5d25f
d3vnu11ch <d3v@nu11.ch>
2016-06-17 17:26:01 +0200

    added "MLV Options" field for mount_mlvfs.lua

--- mount_mlvfs.lua
+++ mount_mlvfs.lua
@@ -1,18 +1,33 @@
 --[[
 MOUNT_MLVFS
+* this plugin will mount/unmount all *.mlv files beneath a given directory on startup/shutdown of darktable.
+* should be useful when treating RAW-Video files recorded with Magic Lantern (http://www.magiclantern.fm/).
+* so far, only tested on Debian 8 and Ubuntu 14.04
 
-Automatically mounts Magic Lantern's *.mlv files through fuse and mlvfs
+AUTHOR
+Fabian Waeber (d3v@nu11.ch)
 
-ADDITIANAL SOFTWARE NEEDED FOR THIS SCRIPT
+ADDITIONAL SOFTWARE NEEDED FOR THIS SCRIPT
 * mlvfs (see https://bitbucket.org/dmilligan/mlvfs for download and howto install)
-* fuse-dev even as a build depency to mlvfs (apt-get install libfuse-dev on Untuntu 14.04)
+* fuse-dev needed as a build depency for mlvfs
+
+INSTALLATION
+* copy this file in $CONFIGDIR/lua/ where CONFIGDIR is your darktable configuration directory
+* require this file from your main lua config file or enable it through "include_all.lua"
+see http://www.darktable.org/usermanual/ch09.html.php#lua_basic_principles for basic darktable & lua usage.
 
 USAGE
-* require this file from your main lua config file or enable it through "include_all.lua":
+configure at least the following three options and restart darktable:
+* MLVFS Binary: Location of the mlvfs binary.
+* MLVFS Source Directory: Directory containing the *.mlv files to mount.
+* MLVFS Destination Directory: Empty (!) target directory.
 
-This plugin will mount/unmount all *.mlv files beneath a given directory on startup/shutdown.
+use any options described at https://bitbucket.org/dmilligan/mlvfs under "MLVFS Options", execpt: "-f" (foreground operation) as it would block darktable startup. Therefore the webinterface of mlvfs will not work with this plugin!
 
+LICENSE
+GPLv3
 ]]
+
 local dt = require "darktable"
 dt.configuration.check_version(...,{3,0,0})
 
@@ -35,6 +50,14 @@ dt.preferences.register(
 
 dt.preferences.register(
   "mount_mlvfs",
+  "mount_mlvfs_opt",
+  "string", "MLVFS Options",
+  "Additional mlvfs parameters - see https://bitbucket.org/dmilligan/mlvfs for complete list.",
+  ""
+)
+
+dt.preferences.register(
+  "mount_mlvfs",
   "mount_mlvfs_bin",
   "file", "MLVFS Binary",
   "Complete path to the mlvfs executable.",
@@ -42,25 +65,25 @@ dt.preferences.register(
 )
 
 -- setup vars
+local mfsbin = dt.preferences.read("mount_mlvfs","mount_mlvfs_bin","string")
 local srcdir = dt.preferences.read("mount_mlvfs","mount_mlvfs_src","string")
 local dstdir = dt.preferences.read("mount_mlvfs","mount_mlvfs_dst","string")
-local mlvfs_mount_cmd = dt.preferences.read("mount_mlvfs","mount_mlvfs_bin","string").." "..dstdir.." --mlv_dir="..srcdir
+local mfsopt = dt.preferences.read("mount_mlvfs","mount_mlvfs_opt","string")
+local mlvfs_mount_cmd = mfsbin.." "..mfsopt.." --mlv_dir="..srcdir.." "..dstdir
 local mlvfs_umount_cmd = "fusermount -u "..dstdir
 
 -- mount on startup
--- dt.print_error(" - MLVFS: executing "..mlvfs_mount_cmd) -- DEBUG ONLY
 local exitval = os.execute(mlvfs_mount_cmd)
 if exitval == true then
   dt.print("Mounted mlvfs source "..srcdir.." to "..dstdir..".")
 else
-  dt.print("ERROR while mounting mlvfs source "..srcdir.." to "..dstdir..".")
+  dt.print("ERROR while mounting mlvfs - start darktable from commandline with \"-d lua\" to see more details.")
 end
   
 -- unmount on shutdown
 dt.register_event(
   "exit",
   function()
-    -- dt.print_error(" - MLVFS: executing "..mlvfs_umount_cmd.." to unmount mlvfs under "..dstdir..".") -- DEBUG ONLY
     local exitval = os.execute(mlvfs_umount_cmd)
   end
 )
